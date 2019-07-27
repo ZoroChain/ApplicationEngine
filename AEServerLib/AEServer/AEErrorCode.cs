d8eda704121824ec6cd4fe4205c15a711f46a1a8
ProDog <xiuboard@foxmail.com>
2019-07-27 20:39:19 +0800

    add error code

--- AEServerLib/AEServer/AEErrorCode.cs
+++ AEServerLib/AEServer/AEErrorCode.cs
@@ -4,14 +4,14 @@ namespace AEServer
     {
         // system error
         public static int ERR_SYS_SERVER_INTERNAL_ERROR = -1;
-
+        public static int ERR_SYS_SERVER_LOGIN_VERIFSIGN_ERROR = -101;
 
         // session error
         public static int ERR_SESSION_NEED_LOGIN        = -1001;
         public static int ERR_SESSION_LOGIN_WRONG_TOKEN = -1002;
         public static int ERR_SESSOIN_PLAYER_NOT_EXIST  = -1003;
         public static int ERR_SESSION_QUEUE_TASK_ERROR  = -1004;
-
+        
 
         // service error
         public static int ERR_SERVICE_NOT_FOUND         = -2001;
